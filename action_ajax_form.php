d50a20dbb99ca99ce3c478563103a4ae4d177a57
Tineeer <31541365+Tineeer@users.noreply.github.com>
2018-02-05 00:59:41 +0200

    Delete action_ajax_form.php

--- action_ajax_form.php
+++
@@ -1,9 +0,0 @@
-<?
-if(isset($_POST['email'])) { $email = $_POST['email']; }
-$db = mysql_connect("localhost", "admin", "admin") 
-or die("Could not connect : " . mysql_error()); //подключение к серверу БД
-mysql_select_db("my_database") or die("Could not select database"); //Выбор базы данных
-$query = 'INSERT INTO table_email (email) VALUES ($POST['email'])';
-mysql_query($query); //добавление имени и мыла
-mysql_close($db); //закрытие соединения
-?>
